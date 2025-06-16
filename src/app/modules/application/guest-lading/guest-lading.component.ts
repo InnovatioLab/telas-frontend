b3d8ec083baf45f110896fa474b3fe8db3a82ef7
kevindsousa <kevindiegodasilvasousa@gmail.com>
2025-06-15 23:43:16 -0300

    refactor: 🔨 separa o scopo da landing page

--- src/app/modules/application/guest-lading/guest-lading.component.ts
+++ src/app/modules/application/guest-lading/guest-lading.component.ts
@@ -1,6 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { GuestHeaderComponent } from '../components/guest-header/guest-header.component';
+import { GuestFooterComponent } from '../components/guest-footer/guest-footer.component';
+import { MapperAnimateComponent } from '../components/mapper-animate/mapper-animate.component';
+import { HowItWorksComponent } from '../components/how-it-works/how-it-works.component';
+import { HeroComponent } from '../components/hero/hero.component';
+import { FeatureComponent } from '../components/feature/feature.component';
 
 @Component({
   selector: 'app-guest-lading',
@@ -8,6 +14,12 @@ import { RouterModule } from "@angular/router";
   imports: [
     CommonModule,
     RouterModule,
+    GuestHeaderComponent,
+    GuestFooterComponent,
+    MapperAnimateComponent,
+    HowItWorksComponent,
+    HeroComponent,
+    FeatureComponent
   ],
   templateUrl: './guest-lading.component.html',
   styleUrls: ['./guest-lading.component.scss']
--- src/app/modules/application/page/view-layout.component.ts
+++ src/app/modules/application/page/view-layout.component.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { GuestHeaderComponent } from '../components/guest-header/guest-header.component';
-import { GuestFooterComponent } from '../components/guest-footer/guest-footer.component';
-import { MapperAnimateComponent } from '../components/mapper-animate/mapper-animate.component';
-import { HowItWorksComponent } from '../components/how-it-works/how-it-works.component';
-import { HeroComponent } from '../components/hero/hero.component';
-import { FeatureComponent } from '../components/feature/feature.component';
 
 @Component({
   selector: 'app-view-layout',
@@ -14,12 +8,6 @@ import { FeatureComponent } from '../components/feature/feature.component';
   imports: [
     CommonModule,
     RouterModule,
-    GuestHeaderComponent,
-    GuestFooterComponent,
-    MapperAnimateComponent,
-    HowItWorksComponent,
-    HeroComponent,
-    FeatureComponent
   ],
   templateUrl: './view-layout.component.html',
   styleUrls: ['./view-layout.component.scss']
