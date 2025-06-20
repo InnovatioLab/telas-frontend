46f4e4a2590d72bd3d25fd86e3f4dfef813c2e5c
kevindsousa <kevindiegodasilvasousa@gmail.com>
2025-06-19 22:29:38 -0300

    refactor: 🔨 ajustando componente de banner

--- src/app/modules/application/components/mapper-animate/mapper-animate.component.ts
+++ src/app/modules/application/components/mapper-animate/mapper-animate.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IconsModule } from '@app/shared/icons/icons.module';
+import { MapsComponent } from '@app/shared/components/maps/maps.component';
 
 @Component({
   selector: 'app-mapper-animate',
   templateUrl: './mapper-animate.component.html',
   styleUrls: ['./mapper-animate.component.scss'],
   standalone: true,
-  imports: [IconsModule],
+  imports: [IconsModule, MapsComponent],
 })
 export class MapperAnimateComponent {}
