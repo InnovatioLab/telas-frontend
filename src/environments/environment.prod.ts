a2f5ebf83b9f4d3323394abd606ae63f4fceff81
kevindsousa <kevindiegodasilvasousa@gmail.com>
2025-07-14 23:47:02 -0300

    fix: 🐛 ajuste em envs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,8 @@
 export interface Environment {
   production: boolean;
   apiUrl: string;
-  emailSuporte: string;
   zipCodeApiKey: string;
   googleMapsApiKey: string;
-  stripePublicKey: string;
-  stripePrivateKey: string;
   nomeToken: string;
   nomeTokenRefresh: string;
 }
@@ -13,11 +10,8 @@ export interface Environment {
 export const environment: Environment = {
   production: true,
   apiUrl: import.meta.env['NG_APP_API'],
-  emailSuporte: import.meta.env['NG_APP_EMAIL_SUPORTE'],
   zipCodeApiKey: import.meta.env['NG_APP_ZIP_CODE_API_KEY'],
   googleMapsApiKey: import.meta.env['NG_APP_GOOGLE_MAPS_API_KEY'],
-  stripePublicKey: import.meta.env['NG_APP_STRIPE_PUBLIC_KEY'],
-  stripePrivateKey: import.meta.env['NG_APP_STRIPE_PRIVATE_KEY'],
   nomeToken: 'diamond_blank_token',
   nomeTokenRefresh: 'diamond_blank_token_refresh',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export interface Environment {
   production: boolean;
   apiUrl: string;
-  emailSuporte: string;
   zipCodeApiKey: string;
   googleMapsApiKey: string;
   stripePublicKey: string;
@@ -13,7 +12,6 @@ export interface Environment {
 export const environment: Environment = {
   production: false,
   apiUrl: import.meta.env['NG_APP_API'],
-  emailSuporte: import.meta.env['NG_APP_EMAIL_SUPORTE'],
   zipCodeApiKey: import.meta.env['NG_APP_ZIP_CODE_API_KEY'],
   googleMapsApiKey: import.meta.env['NG_APP_GOOGLE_MAPS_API_KEY'],
   stripePublicKey: import.meta.env['NG_APP_STRIPE_PUBLIC_KEY'],
