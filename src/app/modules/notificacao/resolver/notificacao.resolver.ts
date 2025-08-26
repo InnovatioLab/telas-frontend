f019d6f6e57570aab71532dd9a8642d11ddfdade
Victor Almeida <victoremmanuelmn@gmail.com>
2025-08-26 10:56:15 -0300

    fix notification resolver

--- src/app/modules/notificacao/resolver/notificacao.resolver.ts
+++ src/app/modules/notificacao/resolver/notificacao.resolver.ts
@@ -1,37 +1,34 @@
-import { Injectable } from '@angular/core';
-import { Resolve } from '@angular/router';
-import { catchError, map, Observable, of } from 'rxjs';
-import { Authentication } from '@raizes-cearenses-nx/authentication-data-access';
-import { Notificacao, NotificacaoState } from '../models';
+import { Injectable } from "@angular/core";
+import { Resolve } from "@angular/router";
+import { Authentication } from "@app/core/service/auth/autenthication";
+import { catchError, map, Observable, of } from "rxjs";
+import { Notification, NotificationState } from "../models";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: "root",
 })
 export class NotificacaoResolver implements Resolve<string | null> {
-    constructor(
-        private notificacaoState: NotificacaoState,
-        private authentication: Authentication
-    ) {}
+  constructor(
+    private notificationState: NotificationState,
+    private authentication: Authentication
+  ) {}
 
-    resolve(): Observable<string | null> {
-        if(!this.authentication._userSignal()) {
-            return null;
-        };
-
-        return this.notificacaoState.listarNaoLidas().pipe(
-            map((response: Notificacao[]) => {
-                if (response && response.length > 0) {
-                    return response.toString();
-                } else {
-                    return null;
-                }
-            }),
-            catchError(() => {
-                return of(null);
-            })
-        )
-
-
-        
+  resolve(): Observable<string | null> {
+    if (!this.authentication._clientSignal()) {
+      return null;
     }
+
+    return this.notificationState.listarNaoLidas().pipe(
+      map((response: Notification[]) => {
+        if (response && response.length > 0) {
+          return response.toString();
+        } else {
+          return null;
+        }
+      }),
+      catchError(() => {
+        return of(null);
+      })
+    );
+  }
 }
