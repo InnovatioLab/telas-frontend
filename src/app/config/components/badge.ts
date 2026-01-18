1c376da8b611d4421ae3cb25e2320d4b8b67efa6
kevindsousa <kevindiegodasilvasousa@gmail.com>
2026-01-18 17:22:36 -0300

    style: ajusta estilos da padge e campo de input lupa

--- src/app/config/components/badge.ts
+++ src/app/config/components/badge.ts
@@ -1,5 +1,8 @@
 export const badge = {
   root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
     fontSize: 'var(--fonte-tamanho-pequeno)',
     fontWeight: 'var(--fonte-peso-medio)',
     padding: '0.25rem 0.5rem',
